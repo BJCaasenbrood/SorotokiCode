ed01ab1faf5aaa09449981e4f5456995ca012c0c
CirclePipe <b.j.caasenbrood@tue.nl>
2020-01-17 13:27:23 +0100

    push

--- config/vernum.m
+++ config/vernum.m
@@ -5,7 +5,7 @@ switch(Request)
     case('gmdl.lib');       x = '3.0';
     case('mesh.lib');       x = '3.0';
     case('fem.lib');        x = '3.0';
-    case('mdl.lib');        x = '3.0';
+    case('model.lib');        x = '3.0';
     otherwise;              x = 0;
 end
 
