7489c0f2d6ad8ce05779256a94bc0d5ae8c90fcb
Caasenbrood <b.j.caasenbrood@tue.nl>
2020-02-19 15:53:04 +0100

    push

--- scripts/fem/fem_beam.m
+++ scripts/fem/fem_beam.m
@@ -3,21 +3,22 @@ clr;
 sdf = @(x) dRectangle(x,0,5,0,1);
 
 msh = Mesh(sdf);
-msh = msh.set('BdBox',[0,5,0,1],'NElem',500);
+msh = msh.set('BdBox',[0,5,0,1],'NElem',250);
       
 msh = msh.generateMesh;
 
 %% add boundary conditions 
 fem = Fem(msh);
-fem = fem.set('TimeStep',1/15);
+fem = fem.set('TimeStep',1/5);
 
 %% add constraint
 fem = fem.AddConstraint('Support',fem.FindNodes('Left'),[1,1]);
-fem = fem.AddConstraint('Load',fem.FindNodes('Right'),[0,-1e-3]);
-fem = fem.AddConstraint('Output',fem.FindNodes('Bottom'),[0,1e-3]);
+fem = fem.AddConstraint('Load',fem.FindNodes('Right'),[1e-3,-1e-3]);
+fem = fem.AddConstraint('Output',fem.FindNodes('Bottom'),[1e-3,1e-3]);
 
 %% select material
 fem.Material = Ecoflex0030;
 
 %% solving
 fem.solve();
+fem.show('Exx')
