6d58b7987249c980c910eca9cf995e10e63e191e
Caasenbrood <b.j.caasenbrood@tue.nl>
2020-06-04 18:58:03 +0200

    push

--- scripts/fem/3D/fem_twist.m
+++ scripts/fem/3D/fem_twist.m
@@ -7,17 +7,15 @@ msh = msh.generate();
 msh = msh.show();
 
 %% generate fem model from mesh
-fem = Fem(msh,'TimeStep',1/10,'PrescribedDisplacement',false);
+fem = Fem(msh,'TimeStep',1/50,'PrescribedDisplacement',false);
 
 %% add constraint
 fem = fem.AddConstraint('Support',fem.FindNodes('Bottom'),[1,1,1]);
-%fem = fem.AddConstraint('Support',fem.FindNodes('Top'),[0,0,0]);
+fem = fem.AddConstraint('Support',fem.FindNodes('Top'),so3([0,0,pi/2]));
 fem = fem.AddConstraint('Load',fem.FindNodes('Top'),[0,1e-1,0]);
-%fem = fem.AddConstraint('Load',fem.FindNodes('Location',[0,-1,6]),[0,-1e-3,0]);
 
 %% select material
 fem.Material =  TPU90();
-%fem.Density = 0.5*ones(msh.NElem,1);
 
 %% solving
 fem.solve();
