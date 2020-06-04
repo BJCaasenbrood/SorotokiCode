027314446677c276c9adbdad8b4d2506ef43eaa0
Caasenbrood <b.j.caasenbrood@tue.nl>
2020-06-04 08:04:16 +0200

    push

--- scripts/gmdl/preview_bunny_slicer.m
+++ scripts/gmdl/preview_bunny_slicer.m
@@ -3,23 +3,16 @@ clr;
 obj = Gmodel('Bunny.stl');
 
 %% set texture
-obj.Texture = grey;
-obj.Emission = [0.75 0.75 0.75];
-
-% obj.set('SSSPower',1.0,'SSSRadius',0.3,'SSS',true,...
-%         'AOPower',1.5,'AORadius',0.3,'AO',true);
-    
-obj.bake().render(); 
-obj.ground;
+obj.bake().render(); obj.ground;
 background(gitpage);
+view(20,20)
 
+%% showing section analysis
 gif('bunny_cut.gif','frame',gcf,'nodither');
-N = 80;
-dx = 80/N;
 x = 80;
 
-for ii = 1:N
+for ii = 1:80
     obj.slice('Z',x);
-    x = x-dx;
+    x = x-1;
     gif;
 end
