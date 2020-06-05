4987b22332e1ce51ae31f7fe7f4ae6fa9813989a
Caasenbrood <b.j.caasenbrood@tue.nl>
2020-06-05 15:03:50 +0200

    push

--- scripts/fem/3D/fem_twist.m
+++ scripts/fem/3D/fem_twist.m
@@ -1,7 +1,7 @@
 clr;
 %% generate mesh from sdf
-sdf = @(x) dCube(x,-1,1,-1,1,0,20);
-msh = Mesh(sdf,'BdBox',[-1,1,-1,1,0,20],'Hexahedron',[4,4,30]);
+sdf = @(x) dCube(x,-1,1,-1,1,0,10);
+msh = Mesh(sdf,'BdBox',[-1,1,-1,1,0,10],'Hexahedron',[4,4,10]);
 
 msh = msh.generate();
 msh = msh.show();
@@ -12,7 +12,7 @@ fem = Fem(msh,'TimeStep',1/50,'PrescribedDisplacement',true);
 %% add constraint
 fem = fem.AddConstraint('Support',fem.FindNodes('Bottom'),[1,1,1]);
 fem = fem.AddConstraint('Support',fem.FindNodes('Top'),[0,0,1]);
-fem = fem.AddConstraint('Load',fem.FindNodes('Top'),so3([0,0,pi/2]));
+fem = fem.AddConstraint('Load',fem.FindNodes('Top'),so3([0,0,pi]));
 
 %% select material
 fem.Material =  TPU90();
