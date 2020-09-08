f80598061a981e72194126df2cc48fedcb7cd728
BJCaasenbrood <b.j.caasenbrood@tue.nl>
2020-09-08 16:53:45 +0200

    Added functionality of generating config.m file from Model.m

--- scripts/mdl/mdl_demo.m
+++ scripts/mdl/mdl_demo.m
@@ -4,11 +4,13 @@ mdl = Model([0,1,1,0,0,0],'NModal',4,'NDisc',2);
 mdl = mdl.set('MovieAxis',[-0.75 0.75 -0.75 0.75 -1.75 .1]*0.85,'Movie',0);
 mdl = mdl.set('Texture',base);
 
+mdl = mdl.set('Point',[1,0,0,0,1,0,0]);
+
 %% generate dynamic model
 mdl = mdl.generate();
 %% simulate soft robot
 mdl = mdl.csolve(); 
-
+% 
 %% show sim
 figure(102)
 t = mdl.get('t');
@@ -25,7 +27,7 @@ subplot(3,4,[3 4 7 8]);
 plot(t,ge(:,5:end),'linewidth',1.0); hold on;
 plot(t,xd(:,5:end),'k--','linewidth',1.0); hold on;
 subplot(3,4,9:12);
-plot(u,'linewidth',1.0); 
+plot(t,u,'linewidth',1.0); 
 
 mdl.showModel();
 
--- src/model/Model.m
+++ src/model/Model.m
@@ -1,26 +1,22 @@
 classdef Model
 
     properties (Access = public)
-        Links;
         Dof;
         NDof;
         NModal;
         NDisc;
-        NLink = 1;
-        tspan = 1;
         Table;
         g;
         ge;
         tau;
-        Controller;
-        Pressure;
         Texture;
         q;
         dq;
         q0;
         dq0;
-        gain;
-        point;
+        Gain;
+        Point;
+        Controller;
     end
     
     properties (Access = private)
@@ -28,24 +24,22 @@ classdef Model
         xd;
         
         t;
-        E;
-        nu = 0.45;
-        mu = 0.75;
+        E, Nu, Mu;
         xia0 = [0,0,0,1,0,0].';
         Phi;
         Ba; Bc; 
-        Mee; Kee; Dee; Qee;
-        Grav;
+        SpaceStep;
+        TimeStep
+        Gravity;
         Radius;
         Density;
-        Length ;
+        Tdomain;
+        Sdomain
+        Length;
         Length0;
-        P1; P2; P3;
-        LumpedMass;
-        Chebyshev;
+        SimTime;
         DistLoad;
-        PressureArea;
-        Jacobian;
+        PArea;
         
         Movie;
         MovieStart;
@@ -58,26 +52,33 @@ methods
 %-------------------------------------------------------------- Model Class
 function obj = Model(Table,varargin) 
     
-    obj.Table = Table;
-    obj.NLink = size(Table,1);
-    obj.LumpedMass = false;
-    obj.Grav = 9.81;
-    obj.Movie = false;
+    obj.Table  = Table;
+    obj.Controller = 1;
+    %obj.NLink = size(Table,1);
+    obj.Movie      = false;
     obj.MovieStart = false;
-    obj.MovieAxis = [];
-    obj.Density = 0.01;
+    obj.MovieAxis  = [];
     
-    obj.Radius = 1e-2;
-    obj.E = 4e2;
-    obj.nu = 0.4;
-    obj.mu = 0.3;
-    obj.Length = 1;
-    obj.NModal = 2;
-    obj.NDisc = 2;
-    obj.Chebyshev = false;
-    obj.PressureArea = 5e-5;
+    obj.NModal    = 2;
+    obj.NDisc     = 2;
+    obj.SpaceStep = 21;
+    obj.TimeStep  = 0.03;
+    obj.Tdomain   = 25;
+    obj.Sdomain   = 1;
+    
+    obj.Density = 0.01;
+    obj.Radius  = 0.01;
+    obj.E       = 1e6;
+    obj.Nu      = 0.4;
+    obj.Mu      = 0.5;
+    obj.Length  = 1;
+    obj.Gravity = -9.81;
+    obj.PArea   = 5e-5;
+    
+    obj.Point = [1,0,0,0,1,0,0];
+    obj.Gain = [5e-4,0.0];
+
     obj.Texture = prusa;
-    obj.Jacobian = true;
     
     for ii = 1:2:length(varargin)
         obj.(varargin{ii}) = varargin{ii+1};
@@ -107,6 +108,7 @@ end
 function Model = generate(Model)
     Model.Length0 = Model.Length;
     Model = GenerateCosserat(Model);
+    Model = GenerateConfigFile(Model);
 end
 
 %---------------------------------------------------------------------- set
@@ -135,10 +137,7 @@ end
 %---------------------------------------------------------------------- set
 function Model = csolve(Model)   
 dir_path = './src/model/tools/solver/build';
-%out = fullfile(dir_path,'input.log');
-% fileID = fopen(out,'w');
-% fprintf(fileID,'%d\n',Model.Pressure(Model.tspan));
-% fclose(fileID);
+
 %//////////////////////////////////
 out = fullfile(dir_path,'log/state.log');
 fileID = fopen(out,'w');
@@ -149,12 +148,9 @@ out = fullfile(dir_path,'log/state_dt.log');
 fileID = fopen(out,'w');
 fprintf(fileID,'%d\n',Model.dq0);
 fclose(fileID);
-%//////////////////////////////////
-% fileID = fopen(out,'w');
-% fprintf(fileID,'%d\n',Model.point);
-% fclose(fileID);
+
 tic
-CMD = 'cd src/model/tools/solver/build && ./solver config.txt';
+CMD = 'cd src/model/tools/solver/build && ./solver config.m';
 system(CMD);
 toc
 y = load('src/model/tools/solver/build/log/state.log');
@@ -355,8 +351,8 @@ for ii = [1:FPS:length(Model.t),length(Model.t)]
          f2 = plotvector(fr,R*[0;0;0.2],'Color',col(3),'linewidth',2,'MaxHeadSize',0.75);
         end
         
-        h = plot3(SweepSE3(:,7),SweepSE3(:,6),-SweepSE3(:,5),'linewidth',1.5,...
-        'Color',col(1));
+        h = plot3(SweepSE3(:,7),SweepSE3(:,6),-SweepSE3(:,5),'linewidth',2,...
+        'Color',col(2));
     
         %hm = plot3(SweepSE3(end,7),SweepSE3(end,6),-SweepSE3(end,5),'.',...
         %   'markersize',10,'Color',col(1));
@@ -370,8 +366,8 @@ for ii = [1:FPS:length(Model.t),length(Model.t)]
     msh.update();
     mshgr.update();
     
-    %title(['T = ',num2str(Model.t(ii),3)]);
-    title(['iterations = ',num2str(ii,3)]);
+    title(['T = ',num2str(Model.t(ii),3)]);
+    %title(['iterations = ',num2str(ii,3)]);
         
     if ~isempty(Model.MovieAxis), axis(Model.MovieAxis); end
     
@@ -397,13 +393,95 @@ I6 = eye(6);
 set = 1:6;
 xa = set(logical(Model.Table));
 xc = setdiff(set,xa);
-Model.Ba = I6(:,xa);
-Model.Bc = I6(:,xc);
+
+Model.Ba   = I6(:,xa);
+Model.Bc   = I6(:,xc);
 Model.NDof = size(Model.Ba,2);
-Model.Phi = @(x) ShapeFunction(Model,x);
-Model.Nq = Model.NDof*Model.NModal;
-Model.q0 = zeros(Model.NDof*Model.NModal,1);
-Model.dq0 = zeros(Model.NDof*Model.NModal,1);
+Model.Phi  = @(x) ShapeFunction(Model,x);
+Model.Nq   = Model.NDof*Model.NModal;
+Model.q0   = zeros(Model.NDof*Model.NModal,1);
+Model.dq0  = zeros(Model.NDof*Model.NModal,1);
+end
+
+%---------------------------------------------------------------------- set
+function Model = GenerateConfigFile(Model)
+   
+global FID;
+File = [cdsoro,'/src/model/tools/solver/build/config.m'];
+delete(File);
+FID = fopen(File,'w');
+
+fprintf(FID,'[options] \n');
+if(Model.Controller)
+    fprintf(FID,'KINEMATIC_CONTROLLER = 0 \n');
+    fprintf(FID,'ENERGY_CONTROLLER    = 1 \n');
+else
+    fprintf(FID,'KINEMATIC_CONTROLLER = 0 \n');
+    fprintf(FID,'ENERGY_CONTROLLER    = 0 \n');
+end
+fprintf(FID,'WRITE_OUTPUT         = 1 \n');
+%fprintf(FID,'POINT_INPUT          = 0 \n');
+
+if(Model.NDisc > 1)
+    fprintf(FID,'DISCONTINIOUS        = 1 \n');
+else
+    fprintf(FID,'DISCONTINIOUS        = 0 \n');
+end
+
+fprintf(FID,'\n[cosserat] \n');
+fprintf(FID,['K1 = ', num2str(Model.Table(1)), '\n']);
+fprintf(FID,['K2 = ', num2str(Model.Table(2)), '\n']);
+fprintf(FID,['K3 = ', num2str(Model.Table(3)), '\n']);
+fprintf(FID,['E1 = ', num2str(Model.Table(4)), '\n']);
+fprintf(FID,['E2 = ', num2str(Model.Table(5)), '\n']);
+fprintf(FID,['E3 = ', num2str(Model.Table(6)), '\n']);
+
+fprintf(FID,'\n[model] \n');
+fprintf(FID,['NMODE   = ', num2str(Model.NModal), '\n']);
+fprintf(FID,['NDISC   = ', num2str(Model.NDisc), '\n']);
+fprintf(FID,['SDOMAIN = ', num2str(Model.Length), '\n']);
+fprintf(FID,['TDOMAIN = ', num2str(Model.Tdomain), '\n']);
+
+fprintf(FID,'\n[solver] \n');
+fprintf(FID,['SPACESTEP = ', num2str(Model.SpaceStep), '\n']);
+fprintf(FID,['TIMESTEP  = ', num2str(Model.TimeStep), '\n']);
+fprintf(FID,['INTSTEP   = ', '100', '\n']);
+fprintf(FID,['ATOL      = ', '1e-2', '\n']);
+fprintf(FID,['RTOL      = ', '1e-2', '\n']);
+fprintf(FID,['LAMBDA    = ', '1e-2', '\n']);
+fprintf(FID,['MAX_IMPL  = ', '-1', '\n']);
+fprintf(FID,['MAX_ITER  = ', '1', '\n']);
+fprintf(FID,['MAX_IK    = ', '1500', '\n']);
+fprintf(FID,['SPEEDUP   = ', '80', '\n']);
+
+fprintf(FID,'\n[physics] \n');
+fprintf(FID,['RHO      = ', num2str(Model.Density), '\n']);
+fprintf(FID,['EMOD     = ', num2str(Model.E), '\n']);
+fprintf(FID,['NU       = ', num2str(Model.Nu), '\n']);
+fprintf(FID,['MU       = ', num2str(Model.Mu), '\n']);
+fprintf(FID,['PRS_AREA = ', '1e-5', '\n']);
+fprintf(FID,['GRAVITY  = ', num2str(Model.Gravity), '\n']);
+fprintf(FID,['RADIUS   = ', '0.01', '\n']);
+fprintf(FID,['AREA     = ', '3.0000e-04', '\n']);
+fprintf(FID,['J_XX     = ', '2.7250e-10', '\n']);
+fprintf(FID,['J_YY     = ', '2.2500e-11', '\n']);
+fprintf(FID,['J_ZZ     = ', '2.5000e-10', '\n']);
+
+fprintf(FID,'\n[control] \n');
+fprintf(FID,['KP = ', num2str(Model.Gain(1)), '\n']);
+fprintf(FID,['KD = ', num2str(Model.Gain(2)), '\n']);
+
+fprintf(FID,'\n[setpoint] \n');
+fprintf(FID,['Q1d = ', num2str(Model.Point(1)), '\n']);
+fprintf(FID,['Q2d = ', num2str(Model.Point(2)), '\n']);
+fprintf(FID,['Q3d = ', num2str(Model.Point(3)), '\n']);
+fprintf(FID,['Q4d = ', num2str(Model.Point(4)), '\n']);
+fprintf(FID,['Xd =  ', num2str(Model.Point(5)), '\n']);
+fprintf(FID,['Yd =  ', num2str(Model.Point(6)), '\n']);
+fprintf(FID,['Zd =  ', num2str(Model.Point(7)), '\n']);
+
+fclose('all');
+    
 end
 
 %---------------------------------------------------------------------- set
@@ -464,149 +542,6 @@ end
 
 end
 
-%---------------------------------------------------------------------- set
-function P = RectDelta(~,x,h)
-P = piecewise(abs(x) < h/2, 1/h, 1);
-end
-
-%---------------------------------------------------------------------- set
-function P = Dirac(~,x,n)
-P = dirac(x,n);
-end
-
-%---------------------------------------------------------------------- set
-function dx = KinematicODE(Model,t,x)
-N = Model.NModal*Model.NDof;
-H = PressureMatrix;
-Model.t = Model.tspan;
-Model.q = x;
-q_   = x(1:N); 
-g0   = [1,0,0,0,0,0,0];
-n0   = [0,0,0,0,0,0];
-
-% static potential/reaction forces
-[~,Qc,yf,ts] = InverseDynamicModel(Model,q_,0*q_,0*q_,g0,n0,n0);
-
-% compute jacobian
-if Model.Jacobian
-[J0,J1] = JacobianCompute(Model,ts, yf(:,1:7));
-end
-
-% compute control action
-if ~isempty(Model.Controller)
-g1  = yf(end,1:7);
-Qd  = -(Adgmap(g1)*J0).'*Model.Controller(Model.t,g1(5:7)');
-F = OptimalControlInput(J0,J1,H,Qd);
-Qs  = [J1.'*H, (J0.'- J1.')*H]*F;
-
-elseif ~isempty(Model.Pressure)
-F = Model.Pressure(Model.t);
-Qs  = [J1.'*H, (J0.'- J1.')*H]*F;
-else
-Qs = Qc*0;
-end
-
-qd_ = q_*0;
-qd_(1) = 1.0;
-
-Qs = 1e-5*(q_ - qd_) - Model.Kee*qd_;
-Qa = Qc + Qs;
-
-dx(:,1) = Model.Dee\(-Qa - Model.Kee*q_);
-end
-
-%---------------------------------------------------------------------- set
-function dx = DynamicODE(Model,t,x)
-N = Model.NModal*Model.NDof;
-H = PressureMatrix;
-Model.q(:,1)  = x(1:N);
-Model.dq(:,1) = x(N+1:end);
-Model.t = t;
-
-q_  = x(1:N);
-dq_ = x(N+1:end);
-g0  = [1,0,0,0,0,0,0];
-n0  = [0,0,0,0,0,0];
-
-% static potential/reaction forces
-[~,Qc] = InverseDynamicModel(Model,q_,0*dq_,0*dq_,g0,n0,n0);
-
-% dynamic coriolis/viscous forces
-[~,Qr,yf,ts] = InverseDynamicModel(Model,q_,dq_,0*dq_,g0,n0,n0);
-Qv = Qr - Qc;
-
-% building the interia matrix
-if ~Model.LumpedMass
-    MC = zeros(N);
-    for ii = 1:(Model.NModal*Model.NDof)
-        [~,Mtmp] = InverseDynamicModel(Model,q_,0*dq_,dalpha(ii,N),g0,n0,n0);
-        MC(:,ii) = -Mtmp + Qc;
-    end
-else
-    MC = Model.Mee;
-
-end
-
-% construct jacobian matrix
-if ~isempty(Model.Controller) || ~isempty(Model.Pressure)
-[J0, J1] = JacobianCompute(Model,ts, yf(:,1:7));
-end
-
-% compute control action
-if ~isempty(Model.Controller)
-g1  = yf(end,1:7);
-Qd  = -(Adgmap(g1)*J0).'*Model.Controller(Model.t,g1(5:7)');
-F = OptimalControlInput(J0,J1,H,Qd);
-Qs  = [J1.'*H, (J0.'- J1.')*H]*F;
-
-elseif ~isempty(Model.Pressure)
-F = Model.Pressure(Model.t);
-Qs  = [J1.'*H, (J0.'- J1.')*H]*F;
-else
-Qs = 0;
-end
-
-A = []; V = []; S = []; D = [];
-
-qd_ = q_*0;
-qd_(1) = 1.0;
-
-Qs = 1e-5*(q_ - qd_) - Model.Kee*qd_;
-
-Qa = Qc + Qv + Qs;
-
-dx = x;
-dx(1:N,1)     = x(N+1:end);
-dx(N+1:end,1) = MC\(-Qa - Model.Kee*q_ -Model.Dee*dq_ );
-
-% delta operator
-function y = dalpha(a,N), y = logical(1:N==a).'; end
-    
-end
-
-%---------------------------------------------------------------------- set
-function [F0,Q0,Y,X] = InverseDynamicModel(Model,q,dq,ddq,g0,eta0,deta0)
-X = linspace(0,Model.Length,1000);
-
-% forward integration
-[~,yf] = oderk(@(t,x) ForwardODE(Model,t,x,q,dq,ddq),X,[g0, eta0 deta0]);
-
-g1    = yf(end,1:7);
-eta1  = yf(end,8:13);
-deta1 = yf(end,14:19);
-
-F1 = zeros(1,6);
-Qa = zeros(Model.Nq,1).';
-
-% backwards integration
-[~,yb] = oderk(@(t,x) BackwardODE(Model,t,x,q,dq,ddq),flip(X),...
-     [g1,eta1,deta1,F1,Qa]);
-
-Y = flipud(yb(:,1:19));
-F0 = -yb(end,20:25).';
-Q0 = yb(end,26:end).';
-end
-
 %--------------------------------------- forwards integration of kinematics
 function dg = ForwardODE(Model,t,g,q,dq,ddq)
 ee = Model.Ba*Model.Phi(t)*q + Model.xia0;
@@ -632,112 +567,10 @@ dg(14:19) = -admap(ee)*deta - admap(dee)*eta + ddee;
 
 end
 
-%----------------------------------------- backward integration of dynamics
-function dg = BackwardODE(Model,t,g,q,dq,ddq)
-ee   = Model.Ba*Model.Phi(t)*q + Model.xia0;
-dee  = Model.Ba*Model.Phi(t)*dq;
-ddee = Model.Ba*Model.Phi(t)*ddq;
 
-Q      = g(1:4);
-r      = g(5:7);
-eta    = g(8:13);
-deta   = g(14:19);
-Lambda = g(20:25);
-Kappa  = ee(1:3);
-Gamma  = ee(4:6);
-
-R    = Quat2Rot(Q);
-A    = StrainMap(R*Kappa(:));
-M    = MassTensor(Model);
-adee = admap(ee);
-adet = admap(eta);
-
-dg = 0*g;
-FBar = zeros(6,1);
-FBar(4:6) = FBar(4:6) - 0*R.'*M(4,4)*[Model.Grav,0,0].';
-
-dg(1:4)    = ((2*norm(Q))^(-1))*A*Q;
-dg(5:7)    = R*Gamma(:);
-dg(8:13)   = -adee*eta + dee;
-dg(14:19)  = -adee*deta - admap(dee)*eta + ddee;
-dg(20:25)  = (adee).'*Lambda + M*deta - adet.'*M*eta - FBar;
-dg(26:end) = -Model.Phi(t).'*Model.Ba.'*Lambda;
-
-end
-
-%-------------------------------------------------- compute jacobian matrix
-function [J0, J1] = JacobianCompute(Model,t,gmat)
-dx = mean(diff(t));
-
-G0 = gmat(1,:);
-GL = gmat(end,:);
-
-J0 = 0.5*dx*localJ(Model,G0,0);
-
-for ii = 1:length(t)-1
-    J0 = J0 + dx*localJ(Model,gmat(ii,:),t(ii));
-    if ii == floor(length(t)/2)-1
-    GMID = gmat(ii+1,:);
-    J1 = J0 + 0.5*dx*localJ(Model,GMID,t(ii+1));
-    J1 = transpose(Adgmap(GMID))*J1;
-    %J1 = J1;
-    end
-end
-
-J0 = transpose(Adgmap(GL))*(J0 + 0.5*dx*localJ(Model,GL,1));
-
-%----------------------------------------------------------------
-function J = localJ(Model,g,t)
-    J = Adgmap(g)*Model.Ba*Model.Phi(t);
-end
-%----------------------------------------------------------------
-    
-end
-  
 end
 end
 
-%----------------------------------------- ODE solver // runge kutta method
-function [t,dx] = oderk(f,t,y0)
-N = length(t);
-F = @(x,y) f(x,y);
-y = zeros(length(y0),N);
-y(:,1) = y0(:);
-h = mean(diff(t));
-
-for i = 1:(N-1)  
-    ti = t(i);
-    yi = y(:,i);
-    
-    k1 = F(ti,yi);
-    k2 = F(ti+(2/3)*h,yi+(2/3)*h*k1);
-    %k3 = F(ti+0.5*h,yi+0.5*h*k2);
-    %k4 = F(ti+h,yi+k3*h);
-    
-    %y(:,i+1) = yi + (1/6)*(k1+2*k2+2*k3+k4)*h;  
-    y(:,i+1) = yi + (1/4)*(k1+3*k2)*h;  
-end
-
-dx = transpose(y);
-t = t(:);
-end
-
-%--------------------------------------- ODE solver // forward euler method
-function [t,dx] = ode1(f,t,y0)
-N = length(t);
-F = @(x,y) f(x,y);
-y = zeros(length(y0),N);
-y(:,1) = y0(:);
-h = mean(diff(t));
-
-for i = 1:(N-1)  
-    k1 = F(t(i),y(:,i));
-    y(:,i+1) = y(:,i) + k1*h;  
-end
-
-dx = transpose(y);
-t = t(:);
-end
 
 %---------------------------------------- isomorhphism between SO(3) and R6
 function y = isomSO3(x)
@@ -757,17 +590,6 @@ g(4:6,4:6) = Wh;
 g(4:6,1:3) = Uh;
 end
 
-%------------------------------------------- adjoint map on lie group SE(3)
-function Adg = Adgmap(g)
-Q = g(1:4);
-r = g(5:7); 
-R = Quat2Rot(Q);
-Adg = zeros(6); 
-Adg(1:3,1:3) = R;
-Adg(4:6,4:6) = R;
-Adg(4:6,1:3) = isomSO3(r)*R;
-end
-
 %----------------------------------------------------------- strain mapping
 function A = StrainMap(K)
 k1 = K(1); k2 = K(2); k3 = K(3);
@@ -783,61 +605,6 @@ Ryx = 2*(x*y + z*w); Ryy = 1 - 2*(x^2 + z^2); Ryz = 2*(y*z - x*w );
 Rzx = 2*(x*z - y*w ); Rzy = 2*(y*z + x*w ); Rzz = 1 - 2 *(x^2 + y^2);
 
 R = [Rxx, Rxy, Rxz; Ryx, Ryy, Ryz; Rzx, Rzy, Rzz];
-end
-
-%----------------------------------------- infinitesimal body strain tensor 
-function H = StrainTensor(Model)
-A = pi*Model.Radius^2;
-J1 = 0.5*pi*Model.Radius^4;
-J2 = 0.25*pi*Model.Radius^4;
-J3 = 0.25*pi*Model.Radius^4;
-E0 = Model.E;
-nu0 = Model.nu;
-G0 = (E0)/(2*(1+nu0));
-H = diag([G0*J1,E0*J2,E0*J3, E0*A, G0*A, G0*A]);
-end
-
-%---------------------------------------- infinitesimal body inertia tensor 
-function M = MassTensor(Model)
-p0 = Model.Density;
-A = pi*Model.Radius^2;
-J1 = 0.5*pi*Model.Radius^4;
-J2 = 0.25*pi*Model.Radius^4;
-J3 = 0.25*pi*Model.Radius^4;
-M = diag([p0*J1,p0*J2,p0*J3,p0*A, p0*A, p0*A]);
-end
-
-%----------------------------------------------------- control input tensor 
-function P = PressureMatrix
-%P1 = [p11;p12;p13];
-A = 5e-6;
-r = 1;
-v = 0;
-
-H = [ 0,              0,             0;
-      0, -0.5*r*sqrt(3), 0.5*r*sqrt(3);
-     -r,          0.5*r,         0.5*r;
-     -v,             -v,            -v;
-      0,              0,             0;
-      0,              0,             0];
-
-P = A*H;
-  
-end
-
-%----------------------------------------------------- control input tensor 
-function Q = OptimalControlInput(J0,J1,H,Qd)
-I = ones(6,1);
-O = zeros(6,1);
-opt = optimoptions('fmincon','Algorithm','interior-point',...
-'MaxIterations',50,'Display','none');
-Aeq = [J1.'*H, (J0.'- J1.')*H]; 
-b = Qd;
-%tau = fmincon(@(x) 0.5*(Aeq*x-b).'*(Aeq*x-b),rand(6,1),[],[],[],[],O,I,[],opt);
-Q = lsqminnorm(Aeq,b);
-%tau = lsqlin(Aeq,b,-eye(6),I*1e-6);
-if isempty(Q), Q = zeros(6,1); end
-
 end
 
 %-------------------------------------------------------------- movie maker
