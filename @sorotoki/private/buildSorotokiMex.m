8f6f6a2bccb1a08ff709edba149682be62e79d05
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2023-10-12 16:17:44 +0200

    Build mex files for curve sweep, forward kinematics, and Lagrangian calculations
    - Added functions to build mex files for curveSweep, forwardKin, and Lagrangian
    - Each function navigates to the appropriate directory and builds the corresponding mex file
    - The build status is logged as either "Build successful" or "Failed"

--- @sorotoki/private/buildSorotokiMex.m
+++ @sorotoki/private/buildSorotokiMex.m
@@ -49,12 +49,51 @@ flag = buildMexLYH;
 fprintf(repmat('\b',1,60));
 log.bool('./sorotokifem/mex/localsYH build:',flag,{'Build succesfull','Failed'});
 
+%% buildCurveSweep
+log.debug('Nagivating towards libary directory');
+cd([installPath, library]);
+
+log.debug('Nagivating towards ..mex/curvesweep/');
+cd('sorotokimodel/mex/curvesweep/');
+
+log.debug('Building mex buildCurveSweep');
+fprintf('./sorotokimodel/mex/curvesweep build: ...');
+flag = buildCurveSweep;
+fprintf(repmat('\b',1,60));
+log.bool('./sorotokimodel/mex/curvesweep build:',flag,{'Build succesfull','Failed'});
+
+%% buildForwardKin
+log.debug('Nagivating towards libary directory');
+cd([installPath, library]);
+
+log.debug('Nagivating towards ..mex/curvesweep/');
+cd('sorotokimodel/mex/forwardkin/');
+
+log.debug('Building mex buildForwardKin');
+fprintf('./sorotokimodel/mex/forwardkin build: ...');
+flag = buildForwardKin;
+fprintf(repmat('\b',1,60));
+log.bool('./sorotokimodel/mex/forwardkin build:',flag,{'Build succesfull','Failed'});
+
+%% buildLagrangian
+log.debug('Nagivating towards libary directory');
+cd([installPath, library]);
+
+log.debug('Nagivating towards ..mex/curvesweep/');
+cd('sorotokimodel/mex/lagrangian/');
+
+log.debug('Building mex buildLagrangian');
+fprintf('./sorotokimodel/mex/lagrangian build: ...');
+flag = buildLagrangian;
+fprintf(repmat('\b',1,60));
+log.bool('./sorotokimodel/mex/lagrangian build:',flag,{'Build succesfull','Failed'});
+
 %% return to main folder
 cd(installPath);
 end
 
 
-%%
+%% buildMexLNH
 function flag = buildMexLNH
     flag = 0;
     try
@@ -75,6 +114,7 @@ function flag = buildMexLNH
     end
 end
 
+%% buildMexLYH
 function flag = buildMexLYH
     flag = 0;
     try
@@ -90,6 +130,67 @@ function flag = buildMexLYH
     codegen -config cfg LocalsYHFast -silent ...
         -args {coder.typeof(10,[inf,inf]), coder.typeof(10,[inf,inf]), coder.typeof(10,[1,1]), coder.typeof(10,[1,1]), coder.typeof(10,[1,1]), coder.typeof(10,[inf,inf]), coder.typeof(10,[inf,1,inf]), coder.typeof(10,[inf,inf,inf]), coder.typeof(10,[inf,1]), coder.typeof(10,[inf,inf]), coder.typeof(10,[inf,inf]), coder.typeof(10,[1,1]), coder.typeof(10,[1,1]), coder.typeof(10,[inf,1]), coder.typeof(10,[3,1]), coder.typeof(10,[3,1])}
         
+    warning on    
+    flag = 1;
+    end
+end
+
+%% buildCurveSweep
+function flag = buildCurveSweep
+    flag = 0;
+    try
+    cfg = coder.config('mex');
+    cfg.IntegrityChecks = false;
+    cfg.ExtrinsicCalls = false;
+    cfg.IntegrityChecks = false;
+    cfg.SaturateOnIntegerOverflow = false;
+    cfg.ResponsivenessChecks = false;
+    cfg.NumberOfCpuThreads = 16;
+    
+    warning off
+    codegen -config cfg curveSweepModifierFast -silent ...
+        -args {coder.typeof(10,[inf,3]), coder.typeof(10,[inf,3]), coder.typeof(10,[inf,1]), coder.typeof(10,[4,4,inf])}
+        
+    warning on    
+    flag = 1;
+    end
+end
+
+%% buildForwardKin
+function flag = buildForwardKin
+    flag = 0;
+    try
+    cfg = coder.config('mex');
+    cfg.IntegrityChecks = false;
+    cfg.ExtrinsicCalls = false;
+    cfg.IntegrityChecks = false;
+    cfg.SaturateOnIntegerOverflow = false;
+    cfg.ResponsivenessChecks = false;
+    cfg.NumberOfCpuThreads = 16;
+    
+    codegen -config cfg computeForwardKinematicsGaussFast ...
+        -args {coder.typeof(10,[inf,1]), coder.typeof(10,[inf,1]), coder.typeof(10,[3,1]), coder.typeof(10,[3,3]), coder.typeof(10,[6,1,inf,inf]), coder.typeof(10,[inf,inf,inf,inf]), coder.typeof(10,[6,inf]), coder.typeof(10,[inf,inf]), coder.typeof(10,[inf,inf])}
+        
+    warning on    
+    flag = 1;
+    end
+end
+
+%% buildLagrangian
+function flag = buildLagrangian
+    flag = 0;
+    try
+    cfg = coder.config('mex');
+    cfg.IntegrityChecks = false;
+    cfg.ExtrinsicCalls = false;
+    cfg.IntegrityChecks = false;
+    cfg.SaturateOnIntegerOverflow = false;
+    cfg.ResponsivenessChecks = false;
+    cfg.NumberOfCpuThreads = 16;
+    
+    codegen -config cfg computeLagrangianGaussFast ...
+        -args {coder.typeof(10,[inf,1]), coder.typeof(10,[inf,1]), coder.typeof(10,[4,4,inf,inf]), coder.typeof(10,[6,inf,inf,inf]), coder.typeof(10,[6,inf,inf,inf]), coder.typeof(10,[inf,inf,inf,inf]), coder.typeof(10,[6,inf]), coder.typeof(10,[6,6,inf,inf]), coder.typeof(10,[6,6,inf,inf]), coder.typeof(10,[6,6,inf,inf]), coder.typeof(10,[3,1]), coder.typeof(10,[inf,inf]), coder.typeof(10,[inf,inf])}
+    
     warning on    
     flag = 1;
     end
