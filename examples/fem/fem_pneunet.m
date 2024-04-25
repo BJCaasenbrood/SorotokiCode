b6a98f41740035c75626977f0e3a72197b56b272
BJCaasenbrood <b.j.caasenbrood@gmail.com>
2024-04-25 10:27:56 +0200

    Update time step in fem_pneunet simulation
    Changed the time step value from 1/60 to 1/1250 for more accuracy in the simulation. Renamed the solve function to simulate for clarity and consistency.

--- examples/fem/fem_pneunet.m
+++ examples/fem/fem_pneunet.m
@@ -2,7 +2,7 @@ clr;
 
 msh = preset.mesh.pneunet();
 
-fem = Fem(msh,'TimeStep',1/60,'BdBox',[-50 120 -100 20]);
+fem = Fem(msh,'TimeStep',1/1250,'BdBox',[-50 120 -100 20]);
 fem = fem.addMaterial(NeoHookean(0.2, 0.33));
 fem = fem.addMaterial(NeoHookean(5., 0.33));
 
@@ -18,7 +18,7 @@ fem.options.LineStyle = '-';
 fem.options.isNonlinear = 1;
 fem.options.Display = @plt;
 
-fem = fem.solve;
+fem = fem.simulate;
 
 function plt(Fem)
     cla;
